1e0f230b83ff892b276de2f302a54e36b5103bac
Nihat <85920289+zacnider@users.noreply.github.com>
2026-03-11 08:18:07 +0300

    updated  new tUSDT address.

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -8,7 +8,7 @@ export const CONTRACTS = {
     ORACLE_PUBKEY: '0x4d7471d00e22bdf5ebdd6cc42353bb64c33f2b35b9bd45700762fa8b4b99e1b9',
     REPUTATION: 'opt1sqzr36zm92pfcwx2aytxcywyrj6yg7k8cdqadxmcj',
     REPUTATION_PUBKEY: '0x0a63528d48a1ea6315427121aacd6aa7f1b5507b90675c82a90ef2de07dabb2c',
-    COLLATERAL_TOKEN: '', // Set to tUSDT FaucetToken address
+    COLLATERAL_TOKEN: 'opt1sqpwe557whharqy2447m7t8y50u0cspahksfn7kgl',
 };
 
 export const CATEGORIES = [
